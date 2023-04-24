60b6e921a6deb38abd914e7a9d78fcef81e9c265
Mavatish <maria.atisha@ug.uchile.com>
2023-04-24 19:20:23 -0400

    se documento

--- src/main/scala/gwent/cartas/cartaClima/CartaClima.scala
+++ src/main/scala/gwent/cartas/cartaClima/CartaClima.scala
@@ -6,11 +6,11 @@ import gwent.cartas.ComunCartas
 import java.util.Objects
 
 /**
- * Esta clase reepresentara las cartas de Clima junto a su Habilidad, esta clase hereda desde ComunCartas
+ * Esta clase reepresentara las cartas de Clima junto a su Habilidad, esta clase hereda desde AbstractCC
  * @param nombrecc
  * @param efecto
  * El método "hashCode" utiliza la clase "Objects" para generar un código hash único para cada instancia de la clase..
- * El método "equals" compara dos instancias de la clase para verificar si estas son iguales. 
+ * El método "equals" compara dos instancias de la clase para verificar si estas son iguales.
  */
 class CartaClima(private val nombrecc: String, private val habilidad: String) extends AbstractCC(nombrecc, habilidad) {
 
